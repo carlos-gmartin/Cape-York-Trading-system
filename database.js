0d7161829ae5bb3c2cdff511966b5f38add36c4c
St-Nischal <102037354+St-Nischal@users.noreply.github.com>
2022-06-08 20:18:32 +0100

    trying a method to create a database

---
+++ database.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3').verbose()
+
+const db = new sqlite3.Database('./database.db',sqlite3.OPEN_READWRITE,(err)=>{
+    if(err) return console.error(err.message);
+    console.log("connection successful")
+})
+
+/** 
+ * Table already exists
+db.run(
+    'Create table users(Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName Varchar(25) Not Null, LastName varchar(25) Not Null, Email varchar(50) Not Null );'
+)
+*/
+module.exports = {db}
