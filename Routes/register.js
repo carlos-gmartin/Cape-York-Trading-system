3c56b5a632f01e96fb29fb6cb7e79697160efe1d
daniel-wright243 <vgb20dsu@uea.ac.uk>
2022-06-12 16:13:41 +0100

    Database Done and POST Request

--- Routes/register.js
+++ Routes/register.js
@@ -53,4 +53,10 @@ router.post('/',[
 
 })
 
+router.post('/getItems', (req, res) => {
+    db.all('SELECT * FROM items WHERE Community = ?',[req.body.Community], (err, results) => {
+        res.send(results);
+    });
+});
+
 module.exports = router
--- database.js
+++ database.js
@@ -5,12 +5,31 @@ const db = new sqlite3.Database('./database.db',sqlite3.OPEN_READWRITE,(err)=>{
     console.log("connection successful")
 })
 
-/** 
- Table already exists
-db.run(
-    'Create table users(Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName Varchar(25) Not Null, LastName varchar(25) Not Null, Email varchar(50) Not Null, Password varchar(50) Not Null );'
-)
-*/
+
+ //Table already exists
+ 
+//db.run(
+//   'Create table users(Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName Varchar(25) Not Null, LastName varchar(25) Not Null, Email varchar(50) Not Null, Password varchar(50) Not Null );'
+//)
+
+//db.run('DROP TABLE items');
+
+  //Table already exists
+  
+  //db.run(
+  //  'Create table items(Id INTEGER PRIMARY KEY AUTOINCREMENT, Community Varchar(25), ItemName Varchar(25) Not Null, ItemCost Varchar(25), ItemDescription Varchar(50), VendorName Varchar(25) Not Null, Contact Varchar(25));'
+  //); 
+
+//db.run('INSERT into items VALUES(?,?,?,?,?,?,?)', [,'plane','Ranger Station', '$1000', 'Turbo Prop Plane', 'Bob', 'Facebook'])
+
+db.all(`
+    SELECT * from items
+`,(err,results)=>{
+    if(err) {return console.error(err.message)}
+    console.log(results)
+})
+
+
 
 db.all(`
     SELECT * from users
